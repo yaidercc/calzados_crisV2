456f425b31a365d56b0f870387b03bb08c21aaa8
yaider <yaidercc@gmail.com>
2020-08-09 21:55:57 -0500

    primer commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Normalize.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <!--contenedor de la pagina-->
+    <div class="contenedor">
+        <!--cabecera de pagina-->
+        <header class="encabezado">
+            <h1 class="">CALZADOS CRIS</h1>
+            <p></p>
+            <a href="#"><i title="notificaciones" class="fas fa-bell"> NOTIFICACIONES</i></a>
+            <a href="#"><i title="notificaciones" class="fas fa-user"> YAIDER</i></a>
+            <a href="#" class="cerrar">SALIR</a>
+        </header>
+
+        <!--filtros-->
+        <div class="filtros">
+            <a href="#">MUJER</a>
+            <a href="#">HOMBRE</a>
+            <a href="#">MAS FILTROS<i class="fas fa-chevron-down"></i></a>
+        </div>
+
+        <!--contenido principal-->
+        <main class="principal">
+            <p>principal</p>
+        </main>
+
+        <!--redes sociales-->
+        <div class="redes">
+
+        </div>
+
+        <!--pie de pagina-->
+        <footer class="pie">
+            <p>pie</p>
+        </footer>
+    </div>
+
+    <!--archivos y links-->
+    <script src='javaScript/main.js'></script> 
+    <script src="https://kit.fontawesome.com/2efdabf6ca.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+    </script>
+</body>
+</html>
